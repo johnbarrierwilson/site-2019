38ebfc68a0f65feb2828aa6ceadc2fc816ffb5f8
John Wilson <johnbarrierwilson@gmail.com>
2019-01-31 16:40:37 -0500

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,18 +15,25 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000000`,
+        theme_color: `#f4400f`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
+    }
+    // {
+    //   resolve: `gatsby-source-dribbble`,
+    //   options: {
+    //     access_token: '<< Your_Access_Token_here >>'
+    //   }
+    // }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',

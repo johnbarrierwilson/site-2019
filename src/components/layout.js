358af5a87bcc8c4039099871e4012817cf8291e5
John Wilson <johnbarrierwilson@gmail.com>
2019-02-08 13:15:14 -0500

    Create basic styles and slide component

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import "../assets/styles/normalize.css"
+import "../assets/styles/application.scss"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -17,21 +17,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        {children}
       </>
     )}
   />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,29 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <div className="slide slide--hero">
+          <div className="slide-content">
+            <h1>Innovative Websites</h1>
+            <p className="tsl">Saving businesses from obscurity.</p>
+          </div>
+        </div>
+        <div className="slide">
+          <div className="slide-content">
+            <h1>A Simple Solution</h1>
+            <p>It's widely understood that websites can be difficult and complicated. I've spent 10 years working on websites for innovative startups so that I can bring that same level of innovation and simplicity to your small business website. Together we can make your website something you are proud to show off.</p>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage

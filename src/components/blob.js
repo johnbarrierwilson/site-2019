5944b50e96fd9013d4321e7da0487d7a426b2ca7
John Wilson <johnbarrierwilson@gmail.com>
2019-02-08 22:28:07 -0500

    Make it WORK!! Woot woot

--- src/components/blob.js
+++ src/components/blob.js
@@ -17,35 +17,50 @@ class Blob extends React.Component {
     this.camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 1000)
     this.camera.position.z = 300
 
-    const light1 = new THREE.DirectionalLight(0x590D82, 0.5);
-    light1.position.set(200, 300, 400); 
+    const light1 = new THREE.DirectionalLight('#f4400f', 0.5);
+    light1.position.set(0, 300, 200); 
     this.scene.add(light1);
 
     const light2 = light1.clone();
-    light2.position.set(-200, 300, 400); 
+    light2.position.set(-400, 300, 200); 
     this.scene.add(light2);
     
-    this.geometry = new THREE.IcosahedronGeometry(210, 0);
+    this.geometry = new THREE.IcosahedronGeometry(170, 5);
     for(var i = 0; i < this.geometry.vertices.length; i++) {
       var vector = this.geometry.vertices[i];
       vector._o = vector.clone();
     }
     const material = new THREE.MeshPhongMaterial({
-      emissive: '#f4400f', 
+      emissive: '#800000', 
       emissiveIntensity: 0.4,
       shininess: 0
     });
     this.shape = new THREE.Mesh(this.geometry, material)
     this.scene.add(this.shape)
 
+    this.animate()
     requestAnimationFrame(this.animate)
   }
 
-  animate = () => {
-    this.shape.rotation.x += 0.005
-    this.shape.rotation.y += 0.005
+  animate = (a) => {
+    this.updateVertices(a)
     this.renderer.render(this.scene, this.camera)
     requestAnimationFrame(this.animate)
+  }
+
+  updateVertices = (a) => {
+    for (var i = 0; i < this.geometry.vertices.length; i++) {
+      var vector = this.geometry.vertices[i];
+      vector.copy(vector._o);
+      var perlin = window.noise.simplex3(
+          (vector.x * 0.002) + (a * 0.0001),
+          (vector.y * 0.002) + (a * 0.0001),
+          (vector.z * 0.002)
+      );
+      var ratio = ((perlin * 0.4) + 0.8);
+      vector.multiplyScalar(ratio);
+    }
+    this.geometry.verticesNeedUpdate = true;
   }
   
   render(){
